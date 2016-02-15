4d14b2c2808aa87ff4e1a650c291d98df661a4fa
Will James <wjames@thoughtworks.com>
2016-02-15 15:05:36 +1100

    Make tests slightly prettier.

--- test.c
+++ test.c
@@ -6,17 +6,20 @@ int add(int,int);
 int main() {
   int sum;
 
-  printf("add(1,2) should equal 3.\n");
+  printf("add(1,2) should equal 3. ");
   sum = add(1,2);
   assert(sum == 3);
+  printf("✓\n");
   
-  printf("add(-5,-5) should equal 0.\n");
+  printf("add(-5,-5) should equal 0. ");
   sum = add(-5, 5);
   assert(sum == 0);
+  printf("✓\n");
   
-  printf("add(-2, -4) should equal -6\n");
+  printf("add(-2, -4) should equal -6. ");
   sum = add(-2, -4);
   assert(sum == -6);
+  printf("✓\n");
   
   printf("tests succeeded.");
   
